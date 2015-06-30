0490b37f5cc714a6a144c854311789cbca551ef1
Yadav Bhattarai <yadavb@microsoft.com>
2015-06-29 18:06:06 -0700

    Adding the collection GET API over ARM in the nuget package for .Net SDK

---
+++ src/ResourceManagement/RemoteApp/RemoteApp.Tests/CollectionTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Azure.Management.RemoteApp;
+using Microsoft.Azure.Management.RemoteApp.Model;
+using Microsoft.Azure.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.Azure.Management.RemoteApp.Tests
+{
+    public class CollectionTests : RemoteAppTestBase
+    {
+        [Fact]
+        public void GetCollectionListTest()
+        {
+            RemoteAppManagementClient raClient = null;
+            ListCollectionOperationResult collections = null;
+
+            using (var undoContext = UndoContext.Current)
+            {
+                undoContext.Start();
+                raClient = GetClient();
+
+                collections = raClient.Collection.List("Microsoft.RemoteApp");
+
+                Assert.NotNull(collections);
+                Assert.Equal(HttpStatusCode.OK, collections.StatusCode);
+                Assert.NotEmpty(collections.Collections);
+
+                foreach (Collection col in collections.Collections)
+                {
+                    Assert.Equal("collections", col.Type);
+                }
+            }
+        }
+
+        //[Fact]
+        //public void GetCollectionTest()
+        //{
+        //    RemoteAppManagementClient raClient = null;
+        //    GetCollectionOperationResult collection = null;
+
+        //    using (var undoContext = UndoContext.Current)
+        //    {
+        //        undoContext.Start();
+        //        raClient = GetClient();
+
+        //        collection = raClient.Collection.Get("Microsoft.RemoteApp", "testcol2");
+
+        //        Assert.NotNull(collection);
+        //        Assert.Equal(HttpStatusCode.OK, collection.StatusCode);
+        //        Assert.NotNull(collection.Collection);
+        //        Assert.NotNull(collection.Collection.Properties);
+        //        Assert.Equal("collections", collection.Collection.Type);
+        //        Assert.Equal("testcol1", collection.Collection.Properties.Name);
+        //    }
+        //}
+    }
+}
